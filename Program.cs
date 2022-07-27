b62f507a6b9a0ab53f8b46248c35e464e70129f1
SeregaSMD <SeregaSMD@example.com>
2022-07-27 10:45:04 +0500

    CodeV1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int CountStringsLessNum(string[] inputArray, int symbolsNum)
+{
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= symbolsNum) count++;
+    }
+    return count;
+}
+
+string[] FilterStringArray(string[] inputArray, int symbolsNum)
+{
+    int stringsNum = CountStringsLessNum(inputArray, symbolsNum);
+    string[] outputArray = new string[stringsNum];
+    int counter = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= symbolsNum)
+        {
+            outputArray[counter] = inputArray[i];
+            counter++;
+        }
+    }
+    return outputArray;
+}
+
+int symbolsNum = 3;
+string[] inputArray = { "af", "acdc", "abc", "ggfd", "dd", "fd" };
+
+string[] outputArray = FilterStringArray(inputArray, symbolsNum);
+
+foreach (string str in outputArray)
+{
+    Console.WriteLine(str);
+}
